e158bc16540f4fe8faa0b13bc3fa7e5c6aa61c0a
kkuei <kevinkuei@gmail.com>
2022-09-01 22:57:27 -0700

    Update GUI layout

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # odin-calculator
 Toy calculator
+
+We'll rely on Javascripts automatic typecasting
+"Your calculator should not evaluate more than a single pair of numbers at a time"
+Trivial implmeentation
+
+
+//psuedocode
+// need to know
+// 1. what the last number is (initialized to zero) 
+// 2. whee we are operating versus providing an operand
+
+
+// if we select a number (0 to 9 or decimal place) when no operator is specified, we overwrite/update the stashed number
+// then if we select an operator, stash the current number, and save the current operator
+
+// if we click another operator, 
+// then we apply the last operator with the current number and the stashed number
+// then we update the display
+// then we stash the current number, and save the current operator
+
+// in this way, this also works for repeatedly hitting the same operator key
+
+
+// number (initialized) ... operator ... operator ... operator ...  operator ...
+// number (initialized) ... number (new) ... operator ... operator ... operator ...
+// number (initialized) ... operator ... number (new) ... operator ... number (new) ... 
+// number (initialized) ... number (new) ... operator ... number (new) ... operator ...
+
+// 1 + 1 - =  --> 0
+// 1 + 1 + =  --> 2
+
+/// make keys pop off if clicked fast enough
--- script.js
+++ script.js
@@ -1,39 +1,105 @@
-const screen = document.getElementById('screen')
-const ac = document.getElementById('ac')
-const plusMinus = document.getElementById('plusminus')
-const percent = document.getElementById('percent')
-const divide = document.getElementById('divide')
-const multiply = document.getElementById('multiply')
-const subtract = document.getElementById('subtract')
-const decimal = document.getElementById('decimal')
-const equal = document.getElementById('equal')
+const display = document.getElementById('display')
+const acBallClearBtn = document.getElementById('ac')
+const plusMinusBtn = document.getElementById('plusminus')
+const percentBtn = document.getElementById('percent')
+const divideBtn = document.getElementById('divide')
+const multiplyBtn = document.getElementById('multiply')
+const subtractBtn = document.getElementById('subtract')
+const addBtn = document.getElementById('add')
+const decimalBtn = document.getElementById('decimal')
+const equalBtn = document.getElementById('equal')
 
-const one = document.getElementById('1')
-const two = document.getElementById('2')
-const three = document.getElementById('3')
-const four = document.getElementById('4')
-const five = document.getElementById('5')
-const six = document.getElementById('6')
-const seven = document.getElementById('7')
-const eight = document.getElementById('8')
-const nine = document.getElementById('9')
+const zeroBtn = document.getElementById('0')
+const oneBtn = document.getElementById('1')
+const twoBtn = document.getElementById('2')
+const threeBtn = document.getElementById('3')
+const fourBtn = document.getElementById('4')
+const fiveBtn = document.getElementById('5')
+const sixBtn = document.getElementById('6')
+const sevenBtn = document.getElementById('7')
+const eightBtn = document.getElementById('8')
+const nineBtn = document.getElementById('9')
 
+let lastNum = display.innerText;
+let currentOperator = null;
+let counter = 0;
 
-window.onclick = (e) => {
-    console.log(e.target.id);
+window.onclick = (e) => updateDisplay(e);
+acBallClearBtn.onclick = () => clickedAllClear();
+addBtn.onclick = () => setOperator(add);
+divideBtn.onclick = () => setOperator(divide);
+multiplyBtn.onclick = () => setOperator(multiply);
+subtractBtn.onclick = () => setOperator(subtract);
 
-    
-    if (e.target.id === 'ac') screen.innerText = '';
+const multiply = (num1, num2) => {return +num1 * +num2}
+const divide = (num1, num2) => {return +num1 / +num2}
+const add = (num1, num2) => {return +num1 + +num2}
+const subtract = (num1, num2) => {return +num1 - +num2}
 
-
-    if (e.target.id === '1') screen.innerText += '1';
-    if (e.target.id === '2') screen.innerText += '2';
-    if (e.target.id === '3') screen.innerText += '3';
-    if (e.target.id === '4') screen.innerText += '4';
-    if (e.target.id === '5') screen.innerText += '5';
-    if (e.target.id === '6') screen.innerText += '6';
-    if (e.target.id === '7') screen.innerText += '7';
-    if (e.target.id === '8') screen.innerText += '8';
-    if (e.target.id === '9') screen.innerText += '9';
-
+function clickedAllClear () {
+    lastNum = 0;
+    display.innerText = lastNum;
+    currentOperator = null;
 }
+
+function stash() {
+    lastNum = display.innerText
+}
+
+function setOperator(operator) {
+    currentOperator = operator;
+}
+
+
+function updateDisplay(e) {
+    // console.log(e.target)
+    // console.log(e.target.id)
+
+    let classes = Array.from(e.target.classList)
+
+    if (classes.indexOf('number') > 0 && e.target.id !== 'ac' && display.innerText === '0') display.innerText = '';
+    if (e.target.id === '0') display.innerText += '0';
+    if (e.target.id === '1') display.innerText += '1';
+    if (e.target.id === '2') display.innerText += '2';
+    if (e.target.id === '3') display.innerText += '3';
+    if (e.target.id === '4') display.innerText += '4';
+    if (e.target.id === '5') display.innerText += '5';
+    if (e.target.id === '6') display.innerText += '6';
+    if (e.target.id === '7') display.innerText += '7';
+    if (e.target.id === '8') display.innerText += '8';
+    if (e.target.id === '9') display.innerText += '9';
+    if ((e.target.id === 'decimal') && (display.innerText.indexOf('.')<0)) {
+        display.innerText += '.';
+    }
+
+    // placeholder for future exploding fun
+    if (e.target.id === '0') counter++;
+    if (counter > 5 ) {
+        zeroBtn.classList.add('button-explode')
+    }
+}
+
+
+
+
+
+
+// if (classes.indexOf('operator') > 0 && currentOperator !== null) {
+//     // console.log(typeof +lastNum)
+//     // console.log(typeof +display.innerText)
+//     let num1 = +lastNum;
+//     let num2 = +display.innerText;
+//     display.innerText = currentOperator(num1, num2);
+
+//     // currentOperator = currentOperato
+//     lastNum = display.innerText;
+// }
+
+// // if operator
+//     // update the stash
+//     // saver the operator
+//     // if have previous operator
+
+// // if number
+//     // update the display
+
